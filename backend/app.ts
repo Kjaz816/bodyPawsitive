2709a443ee83da3d69453583ceefbd2871fe6d7f
Kjaz816 <79782299+Kjaz816@users.noreply.github.com>
2023-05-08 17:25:25 +1200

    removed cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,17 +1,12 @@
 import express from "express";
-import mongoose from "mongoose";
-
-import cors from "cors";
 import "dotenv/config"
 
 
 const app = express();
-// middleware
-const corsOptions = {
-    origin: "https://bodypositiveclient.onrender.com/" // frontend URI (ReactJS)
-}
+
+
 app.use(express.json());
-app.use(cors(corsOptions));
+
 
 // route
 app.get("/", (req, res) => {
