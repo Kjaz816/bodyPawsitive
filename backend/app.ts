588810b7dc649626b61384a2b808bf4763d65412
Kjaz816 <79782299+Kjaz816@users.noreply.github.com>
2023-05-08 15:43:00 +1200

    edit file  system

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from "mongoose";
-const app = express();
+
 import cors from "cors";
 import "dotenv/config"
 
+
+const app = express();
 // middleware
 const corsOptions = {
     origin: "http://localhost:3000" // frontend URI (ReactJS)
@@ -11,17 +13,9 @@ const corsOptions = {
 app.use(express.json());
 app.use(cors(corsOptions));
 
-// connect MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
-    const PORT = process.env.PORT || 8000
-    app.listen(PORT, () => {
-        console.log(`App is Listening on PORT ${PORT}`);
-    })
-}).catch(err => {
-    console.log(err);
-});
-
 // route
 app.get("/", (req, res) => {
     res.status(201).json({message: "Connected to Backend!"});
 });
+
+export default app;
