ccaf4530b092ea933a2efe8f6c113bec31af14f7
Kjaz816 <79782299+Kjaz816@users.noreply.github.com>
2023-05-08 19:33:08 +1200

    added error handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ const App = () => {
 
     console.log(body)
 
-    
+
 
     fetch("https://bodypositive.onrender.com/api/users/signUp", {
       method: "POST",
@@ -33,20 +33,21 @@ const App = () => {
       body: JSON.stringify(body),
     })
       .then((res) => res.json())
-      .then((data) => setReqRes(data));
+      .then((data) => setReqRes(data))
+      .catch((error) => console.error(error));
   };
 
 
   useEffect(() => {
     checkConn();
-  },[]);
+  }, []);
 
   return (
-  <div className="App">
-    <h1>{message}</h1>
-    <button onClick={addUser}>Add User</button>
-    <h1>{reqRes}</h1>
-  </div>
+    <div className="App">
+      <h1>{message}</h1>
+      <button onClick={addUser}>Add User</button>
+      <h1>{reqRes}</h1>
+    </div>
   );
 }
 
