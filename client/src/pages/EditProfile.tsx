e87c7aebbe65fafbb70ed0c384e600d54e41edfb
Kjaz816 <79782299+Kjaz816@users.noreply.github.com>
2023-05-15 01:13:54 +1200

    Change photo implemented

--- client/src/pages/EditProfile.tsx
+++ client/src/pages/EditProfile.tsx
@@ -15,6 +15,7 @@ const EditProfile = () => {
         lastName: "",
         permLevel: "volunteer",
         email: "",
+        photo: "",
         animals: [
             {
                 name: "",
@@ -46,6 +47,7 @@ const EditProfile = () => {
                             lastName: data.lastName,
                             permLevel: data.permLevel,
                             email: data.email,
+                            photo: data.photo,
                             animals: data.animals
                         }
                     );
@@ -56,10 +58,30 @@ const EditProfile = () => {
         }
     };
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
-        setProfileDetails((prevState) => ({ ...prevState, [name]: value }));
+        if (name === "photo") {
+            const file = event.target.files![0];
+            const photoBase64 = await fileToBase64(file);
+            const photoString = (photoBase64 as string).toString().replace(/^data:image\/[a-z]+;base64,/, "");   // remove the file type prefix
+            setProfileDetails((prevState) => ({ ...prevState, [name]: photoString }));
+        } else {
+            setProfileDetails((prevState) => ({ ...prevState, [name]: value }));
+        }
     };
+
+    function fileToBase64(file: any) {
+        return new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(file);
+            fileReader.onload = () => {
+                resolve(fileReader.result);
+            };
+            fileReader.onerror = (e) => {
+                reject(e);
+            };
+        });
+    }
 
     const [updateResponse, setUpdateResponse] = useState<string>()
 
@@ -122,6 +144,8 @@ const EditProfile = () => {
                     value={profileDetails.email || ''}
                     onChange={handleChange}
                 />
+
+                <input type="file" id="changePhoto" name="photo" accept="image/*" onChange={handleChange} />
                 <p> {updateResponse} </p>
 
                 <button onClick={updateProfile}>Update Profile</button>
