a87c8deb5b5aef66f9713ea1d44982a94b93df90
Kjaz816 <79782299+Kjaz816@users.noreply.github.com>
2023-05-18 15:11:53 +1200

    Update SignIn.tsx

--- client/src/pages/SignIn.tsx
+++ client/src/pages/SignIn.tsx
@@ -14,7 +14,7 @@ const SignIn = () => {
             .then((data) => {
                 sessionStorage.setItem("loggedInUser", data.username);
                 sessionStorage.setItem("loggedInUserPermLevel", data.permLevel);
-                window.location.href = "/";
+                window.location.href = "/Profile";
             })
             .catch((error) => {
                 setLoginFailedMessage("Username or password is incorrect");
